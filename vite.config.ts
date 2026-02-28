7e9a0050aa5469ff3d372447f02f79f17c835a7b
Adam Gyulavari <adam@polygence.org>
2026-02-28 22:23:28 +0100

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/link-love-fund/",
   server: {
     host: "::",
     port: 8080,
